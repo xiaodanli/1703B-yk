7a735c2300748b3a89c714cab05f0bd640e17c4c
lixd <dandan580826@163.com>
2019-08-03 11:17:34 +0800

    路由和swiper

---
+++ vue-yk/src/views/home/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="wrap">
+        <router-view class="con"></router-view>
+        <footer>
+            <router-link to="/home/send"  tag="span">派送</router-link>
+            <router-link to="/home/classify"  tag="span">分类</router-link>
+            <router-link to="/home/vip"  tag="span">会员</router-link>
+            <router-link to="/home/car"  tag="span">购物车</router-link>
+            <router-link to="/home/my"  tag="span">我的</router-link>
+        </footer>
+    </div>
+</template>
+
+<style lang="scss">
+    .wrap{
+        width:100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .con{
+        width:100%;
+        flex:1;
+    }
+
+    footer{
+        width:100%;
+        height:44px;
+        background: #eee;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+    .active{
+        color:red;
+    }
+</style>
+
---
+++ vue-yk/src/views/send/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <header>
+            <span>送至：</span>
+        </header>
+        <swiper :options="swiperOption" ref="mySwiper">
+            <!-- slides -->
+            <swiper-slide>
+                <img src="../../assets/logo.png" alt="">
+            </swiper-slide>
+            <swiper-slide>
+                <img src="../../assets/logo.png" alt="">
+            </swiper-slide>
+            <swiper-slide>
+                <img src="../../assets/logo.png" alt="">
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            swiperOption:{}
+        }
+    }
+}
+</script>
